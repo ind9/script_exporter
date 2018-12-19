ccd3d632d5dfd8fee9ba6e252dd4b90326be951e
Aayush Kumar <aayush@indix.com>
2018-12-19 12:16:04 +0530

    Adding support for directory of config files

--- script_exporter_test.go
+++ script_exporter_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
+	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
 var config = &Config{
@@ -10,6 +15,139 @@ var config = &Config{
 		{"failure", "exit 1", 1},
 		{"timeout", "sleep 5", 2},
 	},
+}
+
+func TestCombineYamlScripts(t *testing.T) {
+	fileContent1 := []byte(`
+scripts:
+ - name: mesos_slave_process_check
+   script: ps waux | grep -q mesos.slave
+ 
+ - name: ssh_port_check
+   script: nc -z -w10 $(hostname -i) 22`)
+
+	fileContent2 := []byte(`
+scripts:
+ - name: apache_process_check
+   script: nc -z -w10 localhost 80
+
+ - name: java_process_check
+   script: nc -z -w10 localhost 8080`)
+
+	tmpFile1, err := ioutil.TempFile("", "testFile1")
+	tmpFile2, err := ioutil.TempFile("", "testFile2")
+
+	tmpFile1.Write(fileContent1)
+	tmpFile2.Write(fileContent2)
+
+	defer os.Remove(tmpFile1.Name())
+	defer os.Remove(tmpFile2.Name())
+
+	fileNames := []string{tmpFile1.Name(), tmpFile2.Name()}
+	combinedConfigs, err := combineYamlScripts(fileNames)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	expectedResult := `scripts:
+- name: mesos_slave_process_check
+  script: ps waux | grep -q mesos.slave
+  timeout: 0
+- name: ssh_port_check
+  script: nc -z -w10 $(hostname -i) 22
+  timeout: 0
+- name: apache_process_check
+  script: nc -z -w10 localhost 80
+  timeout: 0
+- name: java_process_check
+  script: nc -z -w10 localhost 8080
+  timeout: 0
+`
+   assert.Equal(t, string(combinedConfigs), expectedResult)
+}
+
+func TestReadYamlsInDirOrFile1(t *testing.T) { // For Directory
+
+	fileContent1 := []byte(`
+scripts:
+ - name: mesos_slave_process_check
+   script: ps waux | grep -q mesos.slave
+ 
+ - name: ssh_port_check
+   script: nc -z -w10 $(hostname -i) 22`)
+
+	fileContent2 := []byte(`
+scripts:
+ - name: apache_process_check
+   script: nc -z -w10 localhost 80
+
+ - name: java_process_check
+   script: nc -z -w10 localhost 8080`)
+
+	dir, err := ioutil.TempDir("/tmp","script_exporter")
+	tmpFile1, err := ioutil.TempFile(dir, "testFile1")
+	tmpFile2, err := ioutil.TempFile(dir, "testFile2")
+
+	tmpFile1.Write(fileContent1)
+	tmpFile2.Write(fileContent2)
+
+	defer os.Remove(tmpFile1.Name())
+	defer os.Remove(tmpFile2.Name())
+	defer os.Remove(dir)
+
+   	finalContent, err := readYamlsinDirOrFile(dir)
+
+   	if err != nil {
+   		fmt.Println("Error: ", err)
+   	}
+
+	expectedResult := `scripts:
+- name: mesos_slave_process_check
+  script: ps waux | grep -q mesos.slave
+  timeout: 0
+- name: ssh_port_check
+  script: nc -z -w10 $(hostname -i) 22
+  timeout: 0
+- name: apache_process_check
+  script: nc -z -w10 localhost 80
+  timeout: 0
+- name: java_process_check
+  script: nc -z -w10 localhost 8080
+  timeout: 0
+`
+   assert.Equal(t, string(finalContent), expectedResult)	
+}
+
+func TestReadYamlsInDirOrFile2(t *testing.T) { // For 
+
+	fileContent1 := []byte(`
+scripts:
+ - name: mesos_slave_process_check
+   script: ps waux | grep -q mesos.slave
+ 
+ - name: ssh_port_check
+   script: nc -z -w10 $(hostname -i) 22`)
+
+	tmpFile1, err := ioutil.TempFile("", "testFile1")
+	tmpFile1.Write(fileContent1)
+	defer os.Remove(tmpFile1.Name())
+
+	finalContent, err := readYamlsinDirOrFile(tmpFile1.Name())
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	expectedResult := `scripts:
+- name: mesos_slave_process_check
+  script: ps waux | grep -q mesos.slave
+  timeout: 0
+- name: ssh_port_check
+  script: nc -z -w10 $(hostname -i) 22
+  timeout: 0
+`
+   assert.Equal(t, string(finalContent), expectedResult)	
 }
 
 func TestRunScripts(t *testing.T) {
